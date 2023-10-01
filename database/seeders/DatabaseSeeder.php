c477a1a73bf1c1dede232ceedb5473550d214a5d
riskiamad <riskiamad@gmail.com>
2023-10-01 23:49:46 +0700

    feat: API Auth & Schedule

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,8 @@ class DatabaseSeeder extends Seeder
         $this->call(
             [
                 UserSeeder::class,
+                SubjectSeeder::class,
+                ScheduleSeeder::class
             ]
         );
     }
